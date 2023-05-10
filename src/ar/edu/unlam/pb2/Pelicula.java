ee4641e0110826390a34e278d4b125570bd2e461
Julian-Gowdak <juliangowdak1@gmail.com>
2023-05-10 11:36:20 -0300

    segundo commit

--- src/ar/edu/unlam/pb2/Pelicula.java
+++ src/ar/edu/unlam/pb2/Pelicula.java
@@ -1,7 +1,90 @@
 package ar.edu.unlam.pb2;
 
+import java.util.List;
+
 public class Pelicula {
-	
+	private String titulo;
+    private String genero;
+    private Integer duracion;
+    private String director;
+    private List<String> reparto;
+    private String formato;
+
+    
+    public Pelicula(String titulo, String genero, Integer duracion, String director, List<String> reparto,String formato) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracion = duracion;
+        this.director = director;
+        this.reparto = reparto;
+        this.setFormato(formato);
+        
+    }
+    
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    
+    public String getGenero() {
+        return genero;
+    }
+    
+    public Integer getDuracion() {
+        return duracion;
+    }
+    
+    public String getDirector() {
+        return director;
+    }
+    
+    public List<String> getReparto() {
+        return reparto;
+    }
+    
+
+	public void agregarActor(String nombre) {
+		this.reparto.add(nombre);
+		
+	}
+
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public void setDuracion(Integer duracion) {
+		this.duracion = duracion;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public void setReparto(List<String> reparto) {
+		this.reparto = reparto;
+	}
+
+
+	public String getDuracionHorasMinutos() {
+		int horas = this.duracion / 60;
+        int minutos = this.duracion % 60;
+        return horas + "h " + minutos + "m";
+	}
+
+
+	public String getFormato() {
+		return formato;
+	}
+
+
+	public void setFormato(String formato) {
+		this.formato = formato;
+	}
 	
 
 }
