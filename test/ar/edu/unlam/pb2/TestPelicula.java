2447c2a576a6b9903cced8c3b3e13e0e559ee9e6
Julian-Gowdak <juliangowdak1@gmail.com>
2023-05-10 11:56:22 -0300

    tercer commit

--- test/ar/edu/unlam/pb2/TestPelicula.java
+++ test/ar/edu/unlam/pb2/TestPelicula.java
@@ -1,5 +1,14 @@
 package ar.edu.unlam.pb2;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestPelicula {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
--- test/ar/edu/unlam/pb2/TestSalaCine.java
+++ test/ar/edu/unlam/pb2/TestSalaCine.java
@@ -1,5 +1,14 @@
 package ar.edu.unlam.pb2;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestSalaCine {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
