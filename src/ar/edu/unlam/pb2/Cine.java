64f9632c23ecafbaf2aaf9570c852d7b9eae8ba1
Julian-Gowdak <juliangowdak1@gmail.com>
2023-05-10 12:14:19 -0300

    Test Cine

--- src/ar/edu/unlam/pb2/Cine.java
+++ src/ar/edu/unlam/pb2/Cine.java
@@ -1,5 +1,56 @@
 package ar.edu.unlam.pb2;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Cine {
+	
+	 private String nombre;
+	   private HashSet<SalaCine> salas;
+
+	    public Cine(String nombre) {
+	        this.nombre = nombre;
+	        this.salas = new HashSet<>();
+	    }
+
+	    public Cine(String nombre, HashSet<SalaCine> listaSalas) {
+			this.nombre = nombre;
+			this.salas = new HashSet <> ();
+			salas = listaSalas;
+		}
+
+		public String getNombre() {
+	        return this.nombre;
+	    }
+
+	    public void agregarSala(SalaCine sala) {
+	        this.salas.add(sala);
+	    }
+
+	    public HashSet<SalaCine> getSalas() {
+	        return this.salas;
+	    }
+
+	    public ArrayList<Pelicula> buscarPeliculasPorGenero(String genero) {
+	        ArrayList<Pelicula> peliculasPorGenero = new ArrayList<>();
+	        for (SalaCine sala : this.salas) {
+	            for (Pelicula pelicula : sala.getPeliculas()) {
+	                if (pelicula.getGenero().equals(genero)) {
+	                    peliculasPorGenero.add(pelicula);
+	                }
+	            }
+	        }
+	        return peliculasPorGenero;
+	    }
+
+	    public HashSet<SalaCine> buscarSalasPorCapacidad(int capacidad) {
+	        HashSet<SalaCine> salasPorCapacidad = new HashSet<>();
+	        for (SalaCine sala : this.salas) {
+	            if (sala.getCapacidad() >= capacidad) {
+	                salasPorCapacidad.add(sala);
+	            }
+	        }
+	        return salasPorCapacidad;
+	    }
 
 }
--- test/ar/edu/unlam/pb2/TestCine.java
+++ test/ar/edu/unlam/pb2/TestCine.java
@@ -2,13 +2,126 @@ package ar.edu.unlam.pb2;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 import org.junit.Test;
 
 public class TestCine {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    public void testAgregarSala2D() {
+        Cine cine = new Cine("Cinepolis");
 
+        SalaCine sala1 = new SalaCine2d(1, 100);
+        cine.agregarSala(sala1);
+
+        SalaCine sala2 = new SalaCine2d(2, 50);
+        cine.agregarSala(sala2);
+
+        assertEquals(2, cine.getSalas().size());
+        assertTrue(cine.getSalas().contains(sala1));
+        assertTrue(cine.getSalas().contains(sala2));
+    }
+	
+	@Test
+    public void testNoSePuedeAgregarSalasConElMismoNumero() {
+        Cine cine = new Cine("Cinepolis");
+
+        SalaCine sala1 = new SalaCine2d(1, 100);
+        cine.agregarSala(sala1);
+
+        SalaCine sala2 = new SalaCine2d(1, 50);
+        cine.agregarSala(sala2);
+
+        assertEquals(1, cine.getSalas().size());
+      
+    }
+	
+	 @Test
+	    public void testAgregarClienteASala2d() {
+	        Cine cine = new Cine("Cinepolis");
+
+	        SalaCine sala1 = new SalaCine2d(1, 100);
+	        cine.agregarSala(sala1);
+	        
+	        Cliente pedro=new Cliente(111111111L, 22,"pedro","apellido");
+	       
+	        Boolean seAgrego=sala1.agregarCliente(pedro);
+			assertTrue(seAgrego);
+	        
+	    }
+	 
+	 @Test
+	    public void testAgregarClienteASala3d() {
+	        Cine cine = new Cine("Cinepolis");
+
+	        SalaCine sala1 = new SalaCine3d(1, 100);
+	        cine.agregarSala(sala1);
+	        
+	        Cliente pedro=new Cliente(111111111L, 22,"pedro","apellido");
+	       
+	        Boolean seAgrego=sala1.agregarCliente(pedro);
+			assertTrue(seAgrego);
+	        
+	    }
+	 
+	 @Test
+	 public void testBuscarSalasPorCapacidad() {
+	     
+	     SalaCine sala1 = new SalaCine2d(1, 100);
+	     SalaCine sala2 = new SalaCine3d(2, 200);
+	     SalaCine sala3 = new SalaCine2d(3, 300);
+
+	     
+	     HashSet<SalaCine> listaSalas = new HashSet<>();
+	     listaSalas.add(sala1);
+	     listaSalas.add(sala2);
+	     listaSalas.add(sala3);
+
+	     
+	     Cine cine = new Cine("Cineplex", listaSalas);
+
+	     
+	     HashSet<SalaCine> resultado = cine.buscarSalasPorCapacidad(200);
+
+	     
+	     assertTrue(resultado.contains(sala2));
+	     assertTrue(resultado.contains(sala3));
+
+	     
+	     assertFalse(resultado.contains(sala1));
+
+	     
+	     assertEquals(2, resultado.size());
+	 }
+	 
+	 @Test
+	 public void testBuscarPeliculasPorGeneroEnSala2D() {
+	     
+	     SalaCine sala1 = new SalaCine2d(1, 100);
+	     
+	     Pelicula pelicula1 = new Pelicula("El Padrino", "Drama", 175, "Francis Ford Coppola", Arrays.asList("Marlon Brando", "Al Pacino", "James Caan"),"2d");
+	     Pelicula pelicula2 = new Pelicula("El Padrino", "Terror", 175, "Francis Ford Coppola", Arrays.asList("Marlon Brando", "Al Pacino", "James Caan"),"2d");
+	     Pelicula pelicula3 = new Pelicula("El Padrino", "Drama", 175, "Francis Ford Coppola", Arrays.asList("Marlon Brando", "Al Pacino", "James Caan"),"2d");
+	     
+	     sala1.agregarPelicula(pelicula1);
+	     sala1.agregarPelicula(pelicula2);
+	     sala1.agregarPelicula(pelicula3);
+
+	     
+	     HashSet<SalaCine> listaSalas = new HashSet<>();
+	     listaSalas.add(sala1);
+	     
+	     
+	     Cine cine = new Cine("Cineplex", listaSalas);
+	     
+	     
+	     ArrayList<Pelicula> resultado = cine.buscarPeliculasPorGenero("Drama");
+	     
+	    
+	     assertEquals(2, resultado.size());
+	     
+	 }
 }
