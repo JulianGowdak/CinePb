5272f20929d066245b1eb31d37079e9c34501e66
facbenitez <facbenitez@alumno.unlam.edu.ar>
2023-06-16 13:59:32 -0300

    se corrigio la clase sala de cine

--- src/ar/edu/unlam/pb2/SalaCine.java
+++ src/ar/edu/unlam/pb2/SalaCine.java
@@ -1,96 +1,110 @@
 package ar.edu.unlam.pb2;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 
 public abstract class SalaCine {
 	
 	private Integer numero;
     private Integer capacidad;
-    private ArrayList<Pelicula> peliculas;
-    private String visualizacion;
+    private TipoDeSala tipo;
+    private HashSet<FuncionDeCine>funcionesEnSalas;
     private HashSet<Cliente>listaDeClientes;
 	
-    public SalaCine(Integer numero, Integer capacidad, String visualizacion) {
-        this.numero = numero;
-        this.capacidad = capacidad;
-        this.setVisualizacion(visualizacion);
-        peliculas = new ArrayList<>();
-        this.listaDeClientes=new HashSet<>(capacidad);
-    }
-    
     public SalaCine(Integer numero, Integer capacidad) {
         this.numero = numero;
         this.capacidad = capacidad;
-        this.visualizacion=" ";
-        peliculas = new ArrayList<>();
-        this.listaDeClientes=new HashSet<>();
-
+        this.funcionesEnSalas = new HashSet<>();
+        this.listaDeClientes=new HashSet<>(capacidad);
     }
     
-
 	public Integer getNumero() {
 		return numero;
 	}
-	public void setNumero(int numero) {
+	public void setNumero(Integer numero) {
 		this.numero = numero;
 	}
 	public Integer getCapacidad() {
 		return capacidad;
 	}
-	public void setCapacidad(int capacidad) {
+	public void setCapacidad(Integer capacidad) {
 		this.capacidad = capacidad;
 	}
 	
-	public ArrayList<Pelicula> getPeliculas() {
-		return peliculas;
-	}
-	public void setPeliculas(ArrayList<Pelicula> peliculas) {
-		this.peliculas = peliculas;
+	public TipoDeSala getTipo() {
+		return tipo;
 	}
 
-	public abstract Boolean agregarPelicula(Pelicula pelicula);
-
-	public abstract Boolean quitarPelicula(Pelicula pelicula);
-	
-	public abstract Boolean agregarCliente(Cliente nuevo) ;
-
-	public abstract Boolean quitarCliente(Cliente nuevo);
-
-
-	public String getVisualizacion() {
-		return visualizacion;
+	public void setTipo(TipoDeSala tipo) {
+		this.tipo = tipo;
 	}
 
-	public void setVisualizacion(String visualizacion) {
-		this.visualizacion = visualizacion;
+	public HashSet<FuncionDeCine> getFuncionesEnSalas() {
+		return funcionesEnSalas;
+	}
+
+	public void setFuncionesEnSalas(HashSet<FuncionDeCine> funcionesEnSalas) {
+		this.funcionesEnSalas = funcionesEnSalas;
 	}
 	
-			
+	public HashSet<Cliente> getListaDeClientes() {
+		return listaDeClientes;
+	}
+
+    public void setListaDeClientes(HashSet<Cliente> listaDeClientes) {
+		this.listaDeClientes = listaDeClientes;
+	}
+
+	public abstract Boolean ingresarFuncionASalasDeCine(FuncionDeCine nuevaFuncion);
+	public abstract Boolean quitarClienteDeSala(Cliente ingresado);
 	
-	 public HashSet<Cliente> getListaDeClientes() {
-			return listaDeClientes;
+	public Boolean quitarFuncionDeSalaDeCine(FuncionDeCine funcion) {
+		Boolean seQuito=false;
+		for (FuncionDeCine funcionDeCine : funcionesEnSalas) {
+			if(funcionDeCine.equals(funcion)) {
+				return funcionesEnSalas.remove(funcion);
+			}
+		}	
+		return seQuito;
+	} 
+		
+	public Boolean agregarCliente(Cliente nuevo) {
+		if(this.listaDeClientes.size()<getCapacidad()) {
+			listaDeClientes.add(nuevo);
+			return true;
 		}
-
-		public void setListaDeClientes(HashSet<Cliente> listaDeClientes) {
-			this.listaDeClientes = listaDeClientes;
+		return false;
+	}
+	
+	public Cliente buscarCliente(Long dni) {
+		for (Cliente clienteActual : listaDeClientes) {
+			if(clienteActual.getDni().equals(dni)) {
+				return clienteActual;
+			}
 		}
-
-		@Override
-		public int hashCode() {
+		return null;
+	}
+	
+	public Integer cantidadDeFuncionesAgregadas() {
+		return this.funcionesEnSalas.size();
+	}	
+	
+	public Integer cantidadDeClientesAgregados() {
+		return this.listaDeClientes.size();
+	}
+	
+	@Override
+	public int hashCode() {
 			final int prime = 31;
 			int result = 1;
 			result = prime * result + ((numero == null) ? 0 : numero.hashCode());
 			return result;
 		}
 
-		@Override
-		public boolean equals(Object obj) {
+	@Override
+	public boolean equals(Object obj) {
 			if (this == obj)
 				return true;
 			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
 				return false;
 			SalaCine other = (SalaCine) obj;
 			if (numero == null) {
