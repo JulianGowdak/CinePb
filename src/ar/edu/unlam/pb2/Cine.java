d3adce4fb3ae795123eb04e1209d9cf51a550f3f
facbenitez <facbenitez@alumno.unlam.edu.ar>
2023-06-16 15:36:25 -0300

    se agregaron listas de funciones y de peliculas, y tambien se corrigio el metodo buscar peliculas por genero

--- src/ar/edu/unlam/pb2/Cine.java
+++ src/ar/edu/unlam/pb2/Cine.java
@@ -6,44 +6,49 @@ import java.util.HashSet;
 public class Cine {
 	
 	 private String nombre;
-	   private HashSet<SalaCine> salas;
+	 private HashSet<SalaCine> salas;
+	 private HashSet <FuncionDeCine>funciones;
+	 private ArrayList <Pelicula>peliculas;
 
-	    public Cine(String nombre) {
+	 public Cine(String nombre) {
 	        this.nombre = nombre;
-	        this.salas = new HashSet<>();
+	        this.salas =new HashSet<>();
+	        this.funciones=new HashSet<>();
+	        this.peliculas=new ArrayList<>();
 	    }
 
-	    public Cine(String nombre, HashSet<SalaCine> listaSalas) {
+	 public Cine(String nombre, HashSet<SalaCine> listaSalas,ArrayList <Pelicula>listaPeliculas) {
 			this.nombre = nombre;
-			this.salas = new HashSet <> ();
+			this.salas =new HashSet<>();
 			salas = listaSalas;
+			this.peliculas =new ArrayList<>();
+			peliculas = listaPeliculas;
+			
 		}
 
-		public String getNombre() {
+	 public String getNombre() {
 	        return this.nombre;
 	    }
 
-		public Boolean agregarSala(SalaCine nuevaSala) {
+	 public Boolean agregarSala(SalaCine nuevaSala) {
 			return this.salas.add(nuevaSala);
 		}
 
-	    public HashSet<SalaCine> getSalas() {
+	 public HashSet<SalaCine> getSalas() {
 	        return this.salas;
 	    }
 
-	    public ArrayList<Pelicula> buscarPeliculasPorGenero(String genero) {
+	 public ArrayList<Pelicula> buscarPeliculasPorGenero(String genero) {
 	        ArrayList<Pelicula> peliculasPorGenero = new ArrayList<>();
-	        for (SalaCine sala : this.salas) {
-	            for (Pelicula pelicula : sala.getPeliculas()) {
-	                if (pelicula.getGenero().equals(genero)) {
-	                    peliculasPorGenero.add(pelicula);
-	                }
-	            }
-	        }
-	        return peliculasPorGenero;
-	    }
+	        for (Pelicula pelicula : this.peliculas) {
+                if (pelicula.getGenero().equals(genero)) {
+                    peliculasPorGenero.add(pelicula);
+                }
+            }
+        return peliculasPorGenero;
+    }
 
-	    public HashSet<SalaCine> buscarSalasPorCapacidad(int capacidad) {
+	 public HashSet<SalaCine> buscarSalasPorCapacidad(Integer capacidad) {
 	        HashSet<SalaCine> salasPorCapacidad = new HashSet<>();
 	        for (SalaCine sala : this.salas) {
 	            if (sala.getCapacidad() >= capacidad) {
@@ -53,21 +58,38 @@ public class Cine {
 	        return salasPorCapacidad;
 	    }
 	    
-	    public Boolean eliminarUnaSala(SalaCine salaAEliminar) {
+	 public Boolean eliminarUnaSala(SalaCine salaAEliminar) {
 	    	Boolean seElimino=false;
 	    	for(SalaCine salaCine:salas){
 	    		if(salas.contains(salaAEliminar)) {
 	    		   if(salaCine.getListaDeClientes().size()==0) {
-	    		      return this.salas.remove(salaAEliminar);
-	    		  
+	    		      return this.salas.remove(salaAEliminar);	    		  
 	    		}
 	    	  }
 	    	}
 	    	 return seElimino;
 	    }
 	    
-	    public Integer cantidadDeSalasAgregadas() {
+	 public Integer cantidadDeSalasAgregadas() {
 	    	return this.salas.size();
 	    }
+
+	public HashSet<FuncionDeCine> getFunciones() {
+		return funciones;
+	}
+
+	public void setFunciones(HashSet<FuncionDeCine> funciones) {
+		this.funciones = funciones;
+	}
+
+	public ArrayList<Pelicula> getPeliculas() {
+		return peliculas;
+	}
+
+	public void setPeliculas(ArrayList<Pelicula> peliculas) {
+		this.peliculas = peliculas;
+	}
+	 
+	 
 
 }
