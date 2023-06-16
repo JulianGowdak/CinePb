91ca38a28e9d523f7d1d2486e2bc1c41bb181cd8
facbenitez <facbenitez@alumno.unlam.edu.ar>
2023-06-16 15:07:02 -0300

    Se corrigio sala de cine 3d y se agregaron nuevos metodos, tambien se utiliza una lista de lentes 3d que va hacer repartidos a cada cliente que entre a la sala y cuando ellos se retiren deben devolverlos para poder retirarse

--- src/ar/edu/unlam/pb2/SalaCine3d.java
+++ src/ar/edu/unlam/pb2/SalaCine3d.java
@@ -1,52 +1,93 @@
 package ar.edu.unlam.pb2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SalaCine3d extends SalaCine{
 	
-	String visualizacion ;
+	private Set<Lente3D>listaDeLentes3D;
 	
 	public SalaCine3d(Integer numero, Integer capacidad) {
 		super(numero, capacidad);
-		setVisualizacion("3d");
+		setTipo(TipoDeSala.SALA_3D);
+		this.listaDeLentes3D=new HashSet<>();
 		
 	}
+	
+	public Set<Lente3D> getListaDeLentes3D() {
+		return listaDeLentes3D;
+	}
 
-	@Override
-	public Boolean agregarPelicula(Pelicula pelicula) {
-		if(pelicula.getFormato()=="3d")
-		super.getPeliculas().add(pelicula);
-		
-		return true;
+	public void setListaDeLentes3D(Set<Lente3D> listaDeLentes3D) {
+		this.listaDeLentes3D = listaDeLentes3D;
 	}
 
 	@Override
-	public Boolean quitarPelicula(Pelicula pelicula) {
-		if(super.getPeliculas().contains(pelicula)) {
-			
-		return super.getPeliculas().remove(pelicula);
-		}
-		return false;
+	public Boolean ingresarFuncionASalasDeCine(FuncionDeCine nuevaFuncion) {
+		Boolean seAgrego=false;
+		if(nuevaFuncion.getVisualizacion().equals(TipoDeVisualizacion.VISUALIZACION_3D)) {
+			super.getFuncionesEnSalas().add(nuevaFuncion);
+			return seAgrego=true;
+		 } 
+	   return seAgrego;
 	}
-
-	@Override
-	public Boolean agregarCliente(Cliente nuevo) {
-		if(super.getListaDeClientes().size()<super.getCapacidad()) {
-			super.getListaDeClientes().add(nuevo);
-			return true;
-		}
+	
+	public Boolean agregarLentes3dASala(Lente3D nuevoLente) {
+		if(this.listaDeLentes3D.size()<super.getCapacidad()) {
+		   return listaDeLentes3D.add(nuevoLente);
+		 }
 		return false;
 	}
 	
+	public Lente3D buscarLente3D(Lente3D lenteABuscar) {
+		for (Lente3D lente3DActual : listaDeLentes3D) {
+			if(lente3DActual.equals(lenteABuscar)) {
+				return lente3DActual;
+			 }
+		 }
+		return null;
+	}
 	
+	public Boolean prestarLente3dACliente(Lente3D aPrestar, Long dniUsuario) {
+		Boolean seRepartio=false;
+		Cliente aRecibirLente=buscarCliente(dniUsuario);
+		
+		for (Lente3D lenteActual : listaDeLentes3D) {
+			 if(lenteActual.equals(aPrestar)) {
+				 lenteActual.setUsuario(aRecibirLente);
+				 return seRepartio=true;
+			}
+		}
+		return seRepartio;
+	}
+
+	public Boolean DevolverLentes3D(Lente3D aDevolver) {
+		Boolean seDevolvio=false;
+		
+		for (Lente3D lenteActual : listaDeLentes3D) {
+			 if(lenteActual.equals(aDevolver)) {
+				lenteActual.setUsuario(null);
+				    return seDevolvio=true;
+			  }
+		      
+		  }
+		return seDevolvio;
+	}
+	
+	public Boolean quitarClienteSala3D(Cliente usuarioIngresado,Boolean DevolverLentes3D) {
+		if(super.getListaDeClientes().contains(usuarioIngresado) && DevolverLentes3D == true) {
+			return super.getListaDeClientes().remove(usuarioIngresado);
+		}
+		return false;
+	
+    }
+
 	@Override
-	public Boolean quitarCliente(Cliente nuevo) {
-		if(super.getListaDeClientes().contains(nuevo)) {
-			
+	public Boolean quitarClienteDeSala(Cliente nuevo) {
+		if(super.getListaDeClientes().contains(nuevo)) {	
 			return super.getListaDeClientes().remove(nuevo);
 		}
 		return false;
 	}
 	
-	
-
-
 }
