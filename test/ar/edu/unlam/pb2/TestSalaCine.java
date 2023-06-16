4b527e54a63833e84f6d5c89fc98a0ff9a46ad5b
facbenitez <facbenitez@alumno.unlam.edu.ar>
2023-06-16 16:46:40 -0300

    se corrigio el test sala de cine y se agrego testQueVerifiqueElFuncionamientoDeAgregarFuncionDelMetodoSala2D y tambien testQueVerifiqueElFuncionamientoDeAgregarFuncionDelMetodoSala3D

--- test/ar/edu/unlam/pb2/TestSalaCine.java
+++ test/ar/edu/unlam/pb2/TestSalaCine.java
@@ -2,6 +2,8 @@ package ar.edu.unlam.pb2;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Arrays;
 
 import org.junit.Test;
@@ -13,7 +15,7 @@ public class TestSalaCine {
 	    SalaCine2d sala = new SalaCine2d(1, 100);
 	    assertEquals((Integer)1, sala.getNumero());
 	    assertEquals((Integer)100, sala.getCapacidad());
-	    assertEquals("2d",sala.getVisualizacion());
+	    
 	}
 	
 	@Test
@@ -21,88 +23,49 @@ public class TestSalaCine {
 	    SalaCine3d sala = new SalaCine3d(1, 100);
 	    assertEquals((Integer)1, sala.getNumero());
 	    assertEquals((Integer)100, sala.getCapacidad());
-	    assertEquals("3d",sala.getVisualizacion());
-	}
-	
-	 @Test
-	    public void testAgregarPeliculasASala2D() {
-
-	        Pelicula pelicula1 = new Pelicula("El Padrino", "Drama", 175, "Francis Ford Coppola", Arrays.asList("Marlon Brando", "Al Pacino", "James Caan"),"2d");
-		    Pelicula pelicula2 = new Pelicula("Matrix", "Ciencia Ficción", 136, "Lana Wachowski y Lilly Wachowski", Arrays.asList("Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"),"2d");
-	      
-	        SalaCine sala1 = new SalaCine2d(1, 100);
-	        
-	        sala1.agregarPelicula(pelicula1);
-	        sala1.agregarPelicula(pelicula2);
-	        
-	        assertEquals(2, sala1.getPeliculas().size());
-	        
-	    }
-	 
-	 @Test
-	    public void testAgregarPeliculasASala3D() {
-
-	        Pelicula pelicula1 = new Pelicula("El Padrino", "Drama", 175, "Francis Ford Coppola", Arrays.asList("Marlon Brando", "Al Pacino", "James Caan"),"3d");
-		    Pelicula pelicula2 = new Pelicula("Matrix", "Ciencia Ficción", 136, "Lana Wachowski y Lilly Wachowski", Arrays.asList("Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"),"3d");
-	      
-	        SalaCine sala1 = new SalaCine3d(1, 100);
-	        
-	        sala1.agregarPelicula(pelicula1);
-	        sala1.agregarPelicula(pelicula2);
-	        
-	        assertEquals(2, sala1.getPeliculas().size());
-	        
-	    }
-	 
-	
-	@Test
-	public void testAgregarPelicula() {
-	    SalaCine sala = new SalaCine2d(1, 100);
-	    Pelicula pelicula = new Pelicula("El Padrino", "Drama", 175, "Francis Ford Coppola", Arrays.asList("Marlon Brando", "Al Pacino", "James Caan"),"2d");
-	    assertTrue(sala.agregarPelicula(pelicula));
+	    
 	}
 	
 	@Test
-	public void testQuitarPelicula() {
-	    SalaCine sala = new SalaCine2d(1, 100);
-	    Pelicula pelicula = new Pelicula("El Padrino", "Drama", 175, "Francis Ford Coppola", Arrays.asList("Marlon Brando", "Al Pacino", "James Caan"),"2d");
-	    sala.agregarPelicula(pelicula);
-	    assertTrue(sala.quitarPelicula(pelicula));
-	    assertTrue(sala.getPeliculas().isEmpty());
+	public void testQueVerifiqueElFuncionamientoDeAgregarFuncionDelMetodoSala2D() {
+	
+		Integer idFuncion1=1;
+		LocalDate fecha1=LocalDate.of(2023, 06, 9);
+		LocalTime horaInicio1=LocalTime.of(15, 30);
+		TipoDeIdioma idioma=TipoDeIdioma.INGLES;
+		TipoDeVisualizacion visualizacion=TipoDeVisualizacion.VISUALIZACION_2D;	
+		
+		SalaCine2d sala1 = new SalaCine2d(1, 100);
+	    Pelicula pelicula = new Pelicula(1,"El Padrino", "Drama", 175, "Francis Ford Coppola", Arrays.asList("Marlon Brando", "Al Pacino", "James Caan"));
+	    FuncionDeCine nuevaFuncion=new FuncionDeCine(idFuncion1, fecha1, horaInicio1, idioma, pelicula, visualizacion);
+	    
+	    Boolean seAgrego=sala1.ingresarFuncionASalasDeCine(nuevaFuncion);
+	    
+	    assertTrue(seAgrego);
 	}
 	
 	@Test
-	public void testQuitarPeliculaNoExistente() {
-	    SalaCine sala = new SalaCine2d(1, 100);
-	    Pelicula pelicula1 = new Pelicula("El Padrino", "Drama", 175, "Francis Ford Coppola", Arrays.asList("Marlon Brando", "Al Pacino", "James Caan"),"2d");
-	    Pelicula pelicula2 = new Pelicula("Matrix", "Ciencia Ficción", 136, "Lana Wachowski y Lilly Wachowski", Arrays.asList("Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"),"2d");
-	    sala.agregarPelicula(pelicula1);
-	    assertFalse(sala.quitarPelicula(pelicula2));
-	    assertEquals(1, sala.getPeliculas().size());
+	public void testQueVerifiqueElFuncionamientoDeAgregarFuncionDelMetodoSala3D() {
+	
+		Integer idFuncion1=1;
+		LocalDate fecha1=LocalDate.of(2023, 06, 9);
+		LocalTime horaInicio1=LocalTime.of(15, 30);
+		TipoDeIdioma idioma=TipoDeIdioma.ESPAÑOL;
+		TipoDeVisualizacion visualizacion=TipoDeVisualizacion.VISUALIZACION_3D;	
+		
+		SalaCine3d sala1 = new SalaCine3d(1, 100);
+	    Pelicula pelicula = new Pelicula(1,"El Padrino", "Drama", 175, "Francis Ford Coppola", Arrays.asList("Marlon Brando", "Al Pacino", "James Caan"));
+	    FuncionDeCine nuevaFuncion=new FuncionDeCine(idFuncion1, fecha1, horaInicio1, idioma, pelicula, visualizacion);
+	    
+	    Boolean seAgrego=sala1.ingresarFuncionASalasDeCine(nuevaFuncion);
+	    
+	    assertTrue(seAgrego);
 	}
-	
-
-	@Test
-	public void testQuePermiteAgregarUnClienteALaSala3d() {
-	    Cliente pedro=new Cliente(111111111L, 22,"pedro","apellido");
-		SalaCine3d sala = new SalaCine3d(1, 100);
-		Boolean seAgrego=sala.agregarCliente(pedro);
-		assertTrue(seAgrego);
-	   	}
-	
-	@Test
-	public void testQuePermiteQuitarUnClienteDeLaSala3d() {
-	    Cliente pedro=new Cliente(111111111L, 22,"pedro","apellido");
-		SalaCine3d sala = new SalaCine3d(1, 100);
-		sala.agregarCliente(pedro);
-		Boolean seAgrego=sala.quitarCliente(pedro);
-		assertTrue(seAgrego);
-	   	}
 	
 	@Test
     public void testQueNoPuedaAgregarClientesCuandoNoHayLugar() {
 
-        SalaCine sala1 = new SalaCine2d(1, 2);
+        SalaCine2d sala1 = new SalaCine2d(1, 2);
         
         Cliente pedro=new Cliente(111111111L, 22,"pedro","apellido");
         Cliente jose=new Cliente(211111111L, 32,"joseo","apellido");
